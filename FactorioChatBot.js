f90b6ad0c9957633c1dd600e27e9f6a35a8f36ce
Kevin Lucas <ktlucas03@gmail.com>
2019-03-14 21:58:56 -0500

    added player logging

--- FactorioChatBot.js
+++ FactorioChatBot.js
@@ -7,17 +7,21 @@ var Rcon = require('rcon');  //npm install rcon
 
 var auth = require('./bot_auth.json');
 var config = require('./config.json');
+//var auth = require('C:\\Users\\Kevin Lucas\\Desktop\\Discord bots\\Factorio Chatbot\\TommyConfig\\bot_auth.json');
+//var config = require('C:\\Users\\Kevin Lucas\\Desktop\\Discord bots\\Factorio Chatbot\\TommyConfig\\config.json');
+
 
 const bot = new Discord.Client();
 const Hook = new webhook.Webhook(config.webHook);
 
 
 fs.writeFile(config.chatLog, '', function(){console.log('clear chat log')});
-
+fs.writeFile(config.playerLog, '', function(){console.log('clear player log')});
 
 bot.login(auth.token);
 
 var conn
+//connect to Rcon
 function RconConnect()
 {
 	conn = new Rcon(config.RconIP, config.RconPort, config.RconPassword);
@@ -27,9 +31,11 @@ function RconConnect()
 		  console.log("Got response: " + str);
 		}).on('end', function() {
 		  console.log("Socket closed!");
+		  //failed to connect try again
 		  RconConnect();
 		}).on('error', function() {
 		  console.log("error");
+		  //failed to connect try again
 		  RconConnect();
 		});
 		console.log("connecting");
@@ -37,19 +43,28 @@ function RconConnect()
 }
 	
  
- 
+//on bot start
 bot.on("ready", () => {
+	//connect to rcon
 	RconConnect();
     console.log('Connected');
     console.log('Logged in as: ');
     console.log(bot.username + ' - (' + bot.id + ')');
 
+	//watch the chat log file for update
 	chokidar.watch(config.chatLog, {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
 		readLastLine(config.chatLog);
 	});
+	
+	//watch the player log file for update
+	chokidar.watch(config.playerLog, {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+		readLastLine(config.playerLog);
+	});
 });
 
+//when we get a message
 bot.on("message", (message) => {
+	//longetr then 0, not a bot, and in channel = channelListen
 	if(message.content.length > 0 && !message.author.bot && message.channel.id === config.channelListen)
 	{
 		console.log(message.content);
@@ -66,6 +81,7 @@ function parseMessage(msg)
 	var index = msg.indexOf(']');
 	if (undefined !== msg && msg.length && index > 1)
 	{
+		//send via webhook, parse name abd message via "[" & "]" characters
 		sendMessage(msg.slice(1,index), msg.slice(index+1));
 	}
 	
@@ -73,16 +89,29 @@ function parseMessage(msg)
 
 function readLastLine(path)
 {
-	fs.readFile(config.chatLog, 'utf-8', function(err, data) {
+	fs.readFile(path, 'utf-8', function(err, data) 
+	{
+		//get last line of file. 
 		if (err) throw err;
 		var lines = data.trim().split('\n');
 		lastLine = lines.slice(-1)[0];
 		console.log(lastLine);
-		//parse the message
-		parseMessage(lastLine);
-		});
+		if(path == config.chatLog && lastLine.length > 0)  //chatlog
+		{
+			//pasrs name and message
+			parseMessage(lastLine);
+		}
+		if(path == config.playerLog  && lastLine.length > 0)  //player join/leave/kill
+		{
+			//use bot to send leave/join message
+			bot.channels.get(config.channelListen).send("**" + lastLine + "**")
+		}		
+	});
+		
+	
 }
 
+//discord webhook
 function sendMessage(name, msg)
 {
 	console.log(msg);
@@ -91,6 +120,8 @@ function sendMessage(name, msg)
                 .setText(msg)
 	Hook.send(send);
 }
+
+
 
 
 
